ebaad7f87bc3a94379e1bb8eeeb6a7dda39d81c2
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-12-06 12:45:30 -0300

    Feat: Counter reducer and action

--- ex/index.jsx
+++ ex/index.jsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import { combineReducers, createStore } from "redux";
 import { Provider } from "react-redux";
 
-import Field from "./field";
-import fieldRedurcer from "./fieldRedurcer";
+import Counter from "./counter";
+import counterReducer from "./counterReducer";
 
 const reducers = combineReducers({
-  field: fieldRedurcer,
+  counter: counterReducer,
 });
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
-    <Field initialValue="Teste" />
+    <Counter />
   </Provider>,
   document.getElementById("app")
 );
